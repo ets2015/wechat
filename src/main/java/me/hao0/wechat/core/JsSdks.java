3d7a9270178b9089e680b2a08c983dcb6bed2c64
郭迁 <qian.guo@wmeimob.com>
2018-01-17 17:28:34 +0800

    增加卡券签名算法（卡券签名和JSSDK的签名完全独立）

--- src/main/java/me/hao0/wechat/core/JsSdks.java
+++ src/main/java/me/hao0/wechat/core/JsSdks.java
@@ -6,7 +6,7 @@ import me.hao0.wechat.model.js.Config;
 import me.hao0.wechat.model.js.Ticket;
 import me.hao0.wechat.model.js.TicketType;
 
-import java.util.Map;
+import java.util.*;
 
 import static me.hao0.common.util.Preconditions.checkNotNull;
 import static me.hao0.common.util.Preconditions.checkNotNullAndEmpty;
@@ -145,5 +145,62 @@ public final class JsSdks extends Component {
         signStr = String.format(signStr, jsApiTicket, nonStr, timestamp, url);
         String sign = Hashing.sha1().hashString(signStr, Charsets.UTF_8).toString().toLowerCase();
         return new Config(wechat.getAppId(), timestamp, nonStr, sign);
+    }
+
+    /**
+     * 获取JSSDK-wxcard卡券调用前的配置信息
+     *
+     * @param accessToken accessToken
+     * @param nonce_str   随机字符串
+     * @param timestamp   时间戳(s)
+     * @param card_id     卡券id
+     * @return
+     */
+    public Config getWxCardConfig(String accessToken, String nonce_str, Long timestamp, String card_id, String openid) {
+        Ticket t = getTicket(accessToken, TicketType.CARD);
+        return getWxCardConfig(t.getTicket(), nonce_str, timestamp, card_id, openid, null);
+    }
+
+    /**
+     * 获取JSSDK-wxcard卡券调用前的配置信息
+     *
+     * @param nonce_str 随机字符串
+     * @param timestamp 时间戳(s)
+     * @param card_id   卡券id
+     * @param openid    用户openid，不填则为""
+     * @param code      卡券code，不填则为""
+     * @return
+     */
+    public Config getWxCardConfig(String nonce_str, Long timestamp, String card_id, String openid, String code) {
+        return getWxCardConfig(wechat.loadTicket(TicketType.CARD), nonce_str, timestamp, card_id, openid, code);
+    }
+
+    /**
+     * 获取JSSDK-wxcard卡券调用前的配置信息
+     *
+     * @param api_ticket jsapi凭证
+     * @param nonce_str  随机字符串
+     * @param timestamp  时间戳(s)
+     * @param card_id    卡券id
+     * @param openid     用户openid，不填则为""
+     * @param code       卡券code，不填则为""
+     * @return
+     */
+    public Config getWxCardConfig(String api_ticket, String nonce_str, Long timestamp, String card_id, String openid, String code) {
+        checkNotNull(api_ticket, "api_ticket can't be null");
+        checkNotNullAndEmpty(nonce_str, "nonStr");
+        checkNotNull(timestamp, "timestamp can't be null");
+        checkNotNull(card_id, "card_id can't be null");
+        List<String> signStrs = new ArrayList<>();
+        signStrs.add(api_ticket);
+        signStrs.add(nonce_str);
+        signStrs.add(timestamp.toString());
+        signStrs.add(card_id);
+        signStrs.add(openid != null ? openid : "");
+        signStrs.add(code != null ? code : "");
+        Collections.sort(signStrs);
+        String signStr = signStrs.get(0) + signStrs.get(1) + signStrs.get(2) + signStrs.get(3) + signStrs.get(4) + signStrs.get(6);
+        String sign = Hashing.sha1().hashString(signStr, Charsets.UTF_8).toString().toLowerCase();
+        return new Config(wechat.getAppId(), timestamp, nonce_str, sign);
     }
 }
