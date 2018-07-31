97d101b2dc55a4576fd9aaeb902687ddc6e9bc2e
z410919244 <410919244@qq.com>
2018-07-31 15:11:38 +0800

    zhoujun

--- src/main/java/me/hao0/wechat/model/store/BaseInfo.java
+++ src/main/java/me/hao0/wechat/model/store/BaseInfo.java
@@ -25,6 +25,10 @@ public class BaseInfo {
     private String introduction;
     private String open_time;
     private String avg_price;
+    /** 门店是否可用状态。1 表示系统错误、2 表示审核中、3 审核通过、4 审核驳回。当该字段为1、2、4 状态时，poi_id 为空 */
+    private Integer available_state;
+    /** 扩展字段是否正在更新中。1 表示扩展字段正在更新中，尚未生效，不允许再次更新； 0 表示扩展字段 没有在更新中或更新已生效 ，可以再次更新 */
+    private Integer update_status;
 
     @Data
     public static class Photo {
